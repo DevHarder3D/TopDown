8fe9a2ccb31b99c376796ebbc1b31e8ee83f9538
IsraelAlvesN <devhardertresd@gmail.com>
2024-01-04 14:35:48 -0300

    Player animations and Flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRB;
+    private Animator playerAnim;
     [SerializeField] private float speed;
     [SerializeField] private Vector2 direction;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
 
     void Update()
     {
         direction = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        if(direction.sqrMagnitude > 0)
+        {
+            playerAnim.SetInteger("Movement", 1);
+        }
+        else
+        {
+            playerAnim.SetInteger("Movement", 0);
+        }
+
+        Flip();
     }
 
     private void FixedUpdate()
     {
         playerRB.MovePosition(playerRB.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        if (direction.x < 0)
+        {
+            transform.eulerAngles = new Vector2 (0, -180);
+        }
+        else if (direction.x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0);
+        }
     }
 }
